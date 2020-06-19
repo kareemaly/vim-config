c91f84a9ca7eb18a79ce4820420935e812ca8fa6
Kareem Elbahrawy <bitriddler@gmail.com>
2020-06-19 21:03:51 +0200

    fix: use coc instead of YCM

--- swap.sh
+++ swap.sh
@@ -1,5 +1,5 @@
 # !/bin/bash
-DIR="$( cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd )"
+DIR="${HOME}/vim-config"
 echo "
 call plug#begin('~/.vim/plugged')
 $(cat "${DIR}/.vimrc-base-plugins")
